9e184deb2c09d616bfed4702513088c06f9552a5
driesvanspauwen <dries.vanspauwen@gmail.com>
2024-06-30 22:40:59 +0200

    add text & gradient background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Coming soon...</h1>
-    </main>
+      <main>
+          <div className="h-screen flex items-center justify-center">
+              <Image
+                  alt="Mountains"
+                  src={"/mesh-gradient.png"}
+                  quality={100}
+                  fill
+                  sizes="100vw"
+                  style={{
+                      objectFit: "cover",
+                      zIndex: "-10",
+                  }}
+              />
+              <div className="">
+                  <h1 className="text-6xl font-bold text-white">DRIES VANSPAUWEN</h1>
+              </div>
+          </div>
+      </main>
   );
 }
