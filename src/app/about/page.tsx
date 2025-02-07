6dca889180ce5d8588c74b67294a520f7f422575
driesvanspauwen <dries.vanspauwen@gmail.com>
2025-02-07 17:25:22 +0100

    add hover card quote

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,6 +8,11 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+} from "@/components/ui/hover-card"
 
 
 export default function About() {
@@ -15,7 +20,8 @@ export default function About() {
         <>
             <TopNavMenu/>
             <div className="flex flex-col items-center justify-center py-20 space-y-20">
-                <div className="w-full max-w-3xl px-8 py-8 rounded-md border border-black dark:border-white items-baseline">
+                <div
+                    className="w-full max-w-3xl px-8 py-8 rounded-md border border-black dark:border-white items-baseline">
                     <div className="flex flex-col space-y-5 justify-center">
                         <h1 className="text-2xl font-bold">About me</h1>
                         <p>
@@ -25,21 +31,37 @@ export default function About() {
                             creativity flow.
                         </p>
                         <p>
-                            I think one of my strengths is my <span className="font-bold">eye for simplicity</span>.
+                            I think one of my strengths is my
+                            <span>
+                                <HoverCard>
+                                  <HoverCardTrigger
+                                      className="font-bold cursor-pointer"> eye for simplicity</HoverCardTrigger>
+                                  <HoverCardContent className="w-full max-w-xl">
+                                      <div className="flex flex-col justify-between space-y-2">
+                                          <p className="text-sm">Simple can be harder than complex; you have to work hard to get your thinking
+                                            clean to make it simple. But it&apos;s worth it in the end because once you get
+                                            there, you can move mountains.</p>
+                                          <p className="text-xs">Steve Jobs - interview, in Business Week 25 May 1998</p>
+                                      </div>
+                                  </HoverCardContent>
+                                </HoverCard>
+                            </span>.
                             My engineering background taught me how ubiquitous over-engineering is and I believe the
                             impact is often underestimated,
                             especially for long-term projects involving multiple people working on the same thing.
                         </p>
                         <p>
-                            Besides spending a lot of time nerding out behind my PC, i regularly go to the gym,
+                            Besides spending a lot of time nerding out behind my PC, I regularly go to the gym,
                             and I love good food and a strong Belgian beer. I enjoy talking ideas over a coffee,
                             so if you ever want to chat about anything, feel free to
-                            <span><a href="https://www.linkedin.com/in/driesvanspauwen" className="font-bold"> reach out</a></span>! :)
+                            <span><a href="https://www.linkedin.com/in/driesvanspauwen"
+                                     className="font-bold"> reach out</a></span>! :)
                         </p>
                         <div className="flex flex-row items-center justify-center pt-5">
                             <Dialog>
                                 <DialogTrigger>
-                                    <Image className="cursor-pointer hover:opacity-80" width={400} height={200} src={"/dries.jpg"} alt={"Me"}></Image>
+                                    <Image className="cursor-pointer hover:opacity-80" width={400} height={200}
+                                           src={"/dries.jpg"} alt={"Me"}></Image>
                                 </DialogTrigger>
                                 <DialogContent className="max-w-3xl w-full">
                                     <DialogHeader>
