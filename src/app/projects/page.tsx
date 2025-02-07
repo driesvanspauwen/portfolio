d6905cd5530173ee8acdb94c5a707673c0ffbc33
driesvanspauwen <dries.vanspauwen@gmail.com>
2025-02-07 19:18:36 +0100

    padding improvements

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,7 +8,7 @@ export default function ProjectsPage() {
     return (
         <>
             <TopNavMenu />
-            <div className="flex flex-col items-center justify-center py-10 space-y-10 w-full px-10">
+            <div className="flex flex-col items-center justify-center py-10 space-y-10 w-full px-5">
                 {projects.map((project) => (
                     <ProjectCard
                         key={project.id}
--- src/components/projects/ProjectDetail.tsx
+++ src/components/projects/ProjectDetail.tsx
@@ -19,7 +19,7 @@ const ProjectDetail: React.FC<ProjectDetailProps> = ({ project }) => {
 
     return (
         <>
-            <div className="flex flex-col items-center justify-center py-5 md:py-20 space-y-5 md:space-y-20 mx-10">
+            <div className="flex flex-col items-center justify-center py-5 md:py-20 space-y-5 md:space-y-20 mx-5">
                 <div className="w-full max-w-3xl px-8 py-8 rounded-md border border-black dark:border-white items-baseline">
                     <div className="flex flex-col space-y-5 justify-center">
                         <div>
